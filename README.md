b94482792ad6f79769546d5f0885691503a83634
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-09-09 21:49:47 -0300

    Updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # EEN903_Java-Web
+
+
+Alunos:
+
+-Henrique Pereira Rosa  		11.02741-0
+
+-Amanda Viviane da Costa Fabri	12.02859-2
+
+-Bruno Veiga					13.04502-4
+
+-Gabriel Cabral Paiva      		15.95033-6
--- aulas/decorator/ex-app/src/app/app.component.ts
+++ aulas/decorator/ex-app/src/app/app.component.ts
@@ -16,23 +16,24 @@ export class AppComponent  implements OnInit {
 
   ngOnInit() {
     print();
-    rotateText(this.msg, 1000);
+    this.rotateText(this.msg, 1000);
   }
 
-}
+  print(): void {
+    console.log('HELLO WORLD Ladys and Gentlemen...');
+  }
 
-function print(): void {
-  console.log('HELLO WORLD Ladys and Gentlemen...');
-}
+  rotateText(msg: string, dt: number) {
+    setInterval(() => this.doRotate(msg), 1000);
+  }
 
-function rotateText(msg: string, dt: number) {
-  setInterval(() => doRotate(this.msg), 1000);
-}
-
-function doRotate(msg) {
-  if (msg.length > 1) {
-    const aux = msg.charAt(msg.length - 1);
-    msg = aux + msg.substring(0, (msg.length - 1));
-    console.log(msg);
+  doRotate(msg) {
+    if (msg.length > 1) {
+      msg = this.msg;
+      const aux = msg.charAt(msg.length - 1);
+      msg = aux + msg.substring(0, (msg.length - 1));
+      console.log(msg);
+      this.msg = msg;
+    }
   }
 }
