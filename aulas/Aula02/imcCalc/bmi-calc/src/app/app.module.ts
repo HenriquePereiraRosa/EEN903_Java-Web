932b8fbbd60bf8d54223fcf464dd2e5aa8ba3c64
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-09-10 22:04:35 -0300

    Added Ionic apps

---
+++ aulas/Aula02/imcCalc/bmi-calc/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { BmiPage } from './../pages/bmi/bmi';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    BmiPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    BmiPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
