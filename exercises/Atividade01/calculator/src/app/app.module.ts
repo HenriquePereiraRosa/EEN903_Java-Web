7e94dc1b8b3f6728c7fba683f7a7611d513eff6a
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-09-11 23:35:14 -0300

    Reorganizaion of files and folders

--- exercises/Atividade01/calculator/src/app/app.module.ts
+++ exercises/Atividade01/calculator/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalcModule } from './calc/calc.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
