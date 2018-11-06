63bf55375d952f7c7067405c0a2e2708308cbbaa
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-11-05 22:08:17 -0200

    Finished Activities.

--- exercises/Atividade02/4Components/src/app/home/home.page.ts
+++ exercises/Atividade02/4Components/src/app/home/home.page.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  brightness = 20;
+
+  constructor(public toastController: ToastController) {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Hello World',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      message: 'Hello World!',
+      showCloseButton: true,
+      position: 'top',
+      closeButtonText: 'Done'
+    });
+    toast.present();
+  }
+
+}
